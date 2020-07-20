a80e3a228f6a90451635193bd934dc5e21585cf7
Igor Vieira <igorvieiradsz@gmail.com>
2020-07-20 11:12:59 -0300

    First commit

---
+++ lib/widgets/favorite_contacts.dart
@@ -0,0 +1,82 @@
+import 'package:chat_ui/models/message_model.dart';
+import 'package:chat_ui/screens/chats_screen.dart';
+import 'package:flutter/material.dart';
+
+class FavoriteContacts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Favorite Contacts',
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.more_horiz),
+                  iconSize: 30,
+                  color: Colors.blueGrey,
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 120,
+            child: ListView.builder(
+              padding: EdgeInsets.only(left: 10),
+              scrollDirection: Axis.horizontal,
+              itemCount: favorites.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => ChatScreen(
+                        user: favorites[index],
+                      ),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 35,
+                          backgroundImage:
+                              AssetImage(favorites[index].imageUrl),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        Text(
+                          favorites[index].name,
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
